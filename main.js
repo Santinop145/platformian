d0c4802c323589b9637981e2b762a21bca3ae596
Santinop145 <santinoalessandrop@gmail.com>
2024-08-16 12:25:31 -0300

    a

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 let character = document.getElementById("iwannabetheguy");
-let upwardPos = character.style.bottom;
-
-setInterval(gravity, 1000);
+let position = window.getComputedStyle(character);
+let altura = position.getPropertyValue("bottom");
+let alturamov = 500;
+let accel = 1;
 
 function gravity(){
-    if(upwardPos > 200){
-        upwardPos -= 9;
+    if(parseInt(altura) > 200){
+        altura = position.getPropertyValue("bottom");
+        accel++;
+        alturamov -= accel;
+        character.style.setProperty("bottom", alturamov.toString() + "px");
+    }
+    else{
+        character.style.setProperty("bottom", "200px");
+        alturamov = 200;
+        accel = 0;
     }
 }
+
+setInterval(gravity, 50);
