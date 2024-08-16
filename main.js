fd7c9ab9934bebf25333a81785f46e43c981e3b0
Santinop145 <santinoalessandrop@gmail.com>
2024-08-16 13:51:41 -0300

    lol

--- main.js
+++ main.js
@@ -4,18 +4,37 @@ let altura = position.getPropertyValue("bottom");
 let alturamov = 500;
 let accel = 1;
 
+
+function isTouchingGround(){
+    if (parseInt(altura) > 200){
+        return true;
+    }
+    return false;
+}
+
 function gravity(){
-    if(parseInt(altura) > 200){
+    if(isTouchingGround()){
         altura = position.getPropertyValue("bottom");
         accel++;
         alturamov -= accel;
         character.style.setProperty("bottom", alturamov.toString() + "px");
     }
     else{
-        character.style.setProperty("bottom", "200px");
-        alturamov = 200;
-        accel = 0;
+        if(accel > 0 || alturamov > 200){
+        accel--;
+        if(accel < 0){
+            accel = -accel;
+        }
+        alturamov += accel;
+        character.style.setProperty("bottom", alturamov.toString() + "px");
+        }
+        else{
+            accel = 0;
+            alturamov = 200;
+            character.style.setProperty("bottom", alturamov.toString() + "px");
+        }
     }
+    console.log(accel);
 }
 
 setInterval(gravity, 50);
