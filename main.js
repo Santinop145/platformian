843b1728263278c4077f70b10abc072839a10de5
Santinop145 <santinoalessandrop@gmail.com>
2024-08-16 21:51:47 -0300

    blub

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 let character = document.getElementById("iwannabetheguy");
+let characterWeapon = document.getElementById("iwannabetheweapon");
 let position = window.getComputedStyle(character);
 let altura = 500;
-let left = 0;
+let left = 400;
 let jumpLimit = 0;
 let alturavel = 1;
 let gravActive = false;
 const accel = 2;
+
+document.addEventListener("keydown", e => characterMove(e))
+document.addEventListener("keydown", e => isJumping(e))
 
 
 function isTouchingGround(){
@@ -15,15 +19,21 @@ function isTouchingGround(){
     return false;
 }
 
-function isJumping(e){
+function updatePosition(){
+    character.style.setProperty("bottom", altura.toString() + "px");
+    character.style.setProperty("left", left.toString() + "px");
+}
+
+setInterval(updatePosition, 25);
+
+async function isJumping(e){
     if(e.keyCode == 87 && !isTouchingGround()){
         jumpInterval = setInterval(() => {
-            alturavel += accel * 2.8;
-            altura += alturavel - jumpLimit * 4;
-            character.style.setProperty("bottom", altura.toString() + "px");
+            alturavel += + accel * 2;
+            altura += alturavel - jumpLimit / 2;
             jumpLimit++;
-            console.log(altura);
-            if(jumpLimit > 14){
+            console.log(alturavel);
+            if(jumpLimit > 8){
                 clearInterval(jumpInterval);
                 alturavel -= 75;
                 jumpLimit = 0;
@@ -31,44 +41,36 @@ function isJumping(e){
         }, 25);
         clearInterval(gravInter);
         gravActive = false;
-        setTimeout(gravity, 400);
+        setTimeout(gravity, 175);
         return true;
     }
     return false;
 }
 
-function characterMove(e){
+async function characterMove(e){
     if(e.keyCode == 68 && left >= 0){
-        left += 5;
-        character.style.setProperty("left", left.toString() + "px");
+        left += 10;
     }
     else if(e.keyCode == 65 && left >= 0){
-        left -= 5;
-        character.style.setProperty("left", left.toString() + "px");
+        left -= 10;
     }
 }
 
-document.addEventListener("keydown", e => characterMove(e))
-document.addEventListener("keydown", e => isJumping(e))
-
-function gravity(){
+async function gravity(){
     if(isTouchingGround()){
         alturavel += accel;
         altura -= alturavel;
-        character.style.setProperty("bottom", altura.toString() + "px");
     }
     else if(alturavel > 1){
         alturavel /= 2;
         altura += alturavel;
-        character.style.setProperty("bottom", altura.toString() + "px");
         }
     else{
         alturavel = 0;
         altura = 200;
-        character.style.setProperty("bottom", altura.toString() + "px");
     }
     if(!gravActive){
-        gravInter = setInterval(gravity, 50)
+        gravInter = setInterval(gravity, 25)
     } 
     gravActive = true;
 }
